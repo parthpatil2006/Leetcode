b33b19aa5f5eb475eea905d949db52905a2e4fbf
Parth Patil <patilvtejas@gmail.com>
2026-07-03 22:17:18 +0530

    Time: 4 ms (31.56%), Space: 8.5 MB (65.59%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        int val = x;
+        long long reverse = 0;
+        while(x  > 0){
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        
+        }
+        return val == reverse;
+    }
+};
